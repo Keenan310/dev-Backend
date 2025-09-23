be66a065ceab9d136cb079de242afa73d042c3ac
Md Kayes Hossan Fuad <kayesthechill@gmail.com>
2025-09-23 23:03:04 +0600

    update refund

--- src/api/flight/flight.service.ts
+++ src/api/flight/flight.service.ts
@@ -376,7 +376,7 @@ export class FlightService {
 
     if(booking.system === 'Sabre'){
       const  ticketdetails=  await this.ticketingRepository.find({ where : {bookingId: booking.bookingId}});
-      const refunddata = await this.refundRepository.findOne({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
+      const refunddata = await this.refundRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
       const reissuedata =  await this.reissueRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
 
 
@@ -394,7 +394,7 @@ export class FlightService {
     }else if(booking.system === 'Portal'){
 
       const  ticketdetails=  await this.ticketingRepository.find({ where : {bookingId: booking.bookingId}});
-      const refunddata = await this.refundRepository.findOne({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
+      const refunddata = await this.refundRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
       const reissuedata =  await this.reissueRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
 
 
@@ -413,7 +413,7 @@ export class FlightService {
 
     }else if(booking.system === 'AlHind'){
       const  ticketdetails=  await this.ticketingRepository.find({ where : {bookingId: booking.bookingId}});
-      const refunddata = await this.refundRepository.findOne({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
+      const refunddata = await this.refundRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
       const reissuedata =  await this.reissueRepository.find({ where : {bookingId: booking.bookingId}, order: { created_at: "DESC" }});
 
 
