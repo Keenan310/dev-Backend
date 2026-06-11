54b2245e430251cb9626217b57906e0d1f07841f
Md Kayes Hossan Fuad <kayesthechill@gmail.com>
2026-06-12 02:42:24 +0600

    update page size

--- src/api/groupfare/groupfare.service.ts
+++ src/api/groupfare/groupfare.service.ts
@@ -169,7 +169,7 @@ export class GroupfareService {
       },
       data: {
         pageIndex: 0,
-        pageSize: 20,
+        pageSize: 200000000000,
         filter: {},
         sortColumns: [],
       },
